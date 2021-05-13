24aea58c7825328b0db6d47f9016841bf10507ba
Chance Griffin <cbg@hey.com>
2021-05-13 19:34:09 -0400

    fyll command

--- lib/jekyll-contentfyll.rb
+++ lib/jekyll-contentfyll.rb
@@ -1,2 +1,6 @@
 require 'jekyll-contentfyll/version'
 require 'jekyll-contentfyll/importer'
+
+%w[fyll].each do |file|
+  require File.expand_path("jekyll/commands/#{file}.rb", File.dirname(__FILE__))
+end
